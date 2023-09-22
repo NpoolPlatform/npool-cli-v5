72ff8f5e09c93715135042d2e99251c74f6a8093
Zhao KK <kikakkz@hotmail.com>
2023-09-22 18:12:14 +0800

    Correct announcements and user

--- notif/announcement/user/const.ts
+++ notif/announcement/user/const.ts
@@ -2,5 +2,5 @@ export enum API {
   CREATE_ANNOUNCEMENTUSER = '/notif/v1/create/announcementuser',
   DELETE_ANNOUNCEMENTUSER = '/notif/v1/delete/announcementuser',
   GET_ANNOUNCEMENTUSERS = '/notif/v1/get/announcementusers',
-  GET_N_APP_ANNOUNCEMENTUSERS = '/notif/v1/get/n/app/announcementusers',
+  GET_APP_ANNOUNCEMENTUSERS = '/notif/v1/get/app/announcementusers',
 }
