b375da1f4ee2fe27672d7e0b8c4856b76d1f9500
lidaqiang <dqli161@163.com>
2023-10-13 16:49:35 +0800

    add coupon scope sdk

--- sdk/allocatedcoupon.ts
+++ sdk/allocatedcoupon.ts
@@ -1,11 +1,11 @@
 import { computed } from 'vue'
-import { allocatedCoupon, constant, notify } from '..'
+import { allocatedcoupon, constant, notify } from '..'
 import { AppID } from './localapp'
 
-const _allocatedCoupon = allocatedCoupon.useAllocatedCouponStore()
+const _allocatedcoupon = allocatedcoupon.useAllocatedCouponStore()
 
 export const getPageAllocatedCoupons = (pageIndex: number, pageEnd: number, done?: (error: boolean, totalPages: number, totalRows: number) => void) => {
-  _allocatedCoupon.getCoupons({
+  _allocatedcoupon.getCoupons({
     Offset: pageIndex * constant.DefaultPageSize,
     Limit: constant.DefaultPageSize,
     Message: {
@@ -16,7 +16,7 @@ export const getPageAllocatedCoupons = (pageIndex: number, pageEnd: number, done
         Type: notify.NotifyType.Error
       }
     }
-  }, (error: boolean, rows?: Array<allocatedCoupon.Coupon>, total?: number) => {
+  }, (error: boolean, rows?: Array<allocatedcoupon.Coupon>, total?: number) => {
     if (error || !rows?.length || (pageEnd > 0 && pageIndex === pageEnd - 1)) {
       const totalPages = Math.ceil(total as number / constant.DefaultPageSize)
       done?.(error, totalPages, total as number)
@@ -30,10 +30,10 @@ export const getAllocatedCoupons = (pageStart: number, pages: number, done?: (er
   getPageAllocatedCoupons(pageStart, pages ? pageStart + pages : pages, done)
 }
 
-export const _allocatedCoupons = computed(() => _allocatedCoupon.coupons(AppID.value))
+export const _allocatedcoupons = computed(() => _allocatedcoupon.coupons(AppID.value))
 
-export const createAllocatedCoupon = (target: allocatedCoupon.Coupon, finish: (error: boolean) => void) => {
-  _allocatedCoupon.createCoupon({
+export const createAllocatedCoupon = (target: allocatedcoupon.Coupon, finish: (error: boolean) => void) => {
+  _allocatedcoupon.createCoupon({
     ...target,
     TargetUserID: target.UserID,
     UserID: undefined as unknown as string,
---
+++ sdk/couponscope.ts
@@ -0,0 +1,77 @@
+import { computed } from 'vue'
+import { couponscope, constant, notify } from '..'
+import { AppID } from './localapp'
+
+const scope = couponscope.useScopeStore()
+
+const getPageScopes = (pageIndex: number, pageEnd: number, done?: (error: boolean, totalPages: number, totalRows: number) => void) => {
+  scope.getScopes({
+    Offset: pageIndex * constant.DefaultPageSize,
+    Limit: constant.DefaultPageSize,
+    Message: {
+      Error: {
+        Title: 'MSG_GET_COUPON_SCOPES',
+        Message: 'MSG_GET_COUPON_SCOPES_FAIL',
+        Popup: true,
+        Type: notify.NotifyType.Error
+      }
+    }
+  }, (error: boolean, rows?: Array<couponscope.Scope>, total?: number) => {
+    if (error || !rows?.length || (pageEnd > 0 && pageIndex === pageEnd - 1)) {
+      const totalPages = Math.ceil(total as number / constant.DefaultPageSize)
+      done?.(error, totalPages, total as number)
+      return
+    }
+    getPageScopes(++pageIndex, pageEnd, done)
+  })
+}
+
+export const getScopes = (pageStart: number, pages: number, done?: (error: boolean, totalPages: number, totalRows: number) => void) => {
+  getPageScopes(pageStart, pages ? pageStart + pages : pages, done)
+}
+
+export const scopes = computed(() => scope.scopes(AppID.value))
+
+export const createScope = (target: couponscope.Scope, finish: (error: boolean) => void) => {
+  scope.createScope({
+    ...target,
+    Message: {
+      Error: {
+        Title: 'MSG_CREATE_COUPON_SCOPE',
+        Message: 'MSG_CREATE_COUPON_SCOPE_FAIL',
+        Popup: true,
+        Type: notify.NotifyType.Error
+      },
+      Info: {
+        Title: 'MSG_CREATE_COUPON_SCOPE',
+        Message: 'MSG_CREATE_COUPON_SCOPE_SUCCESS',
+        Popup: true,
+        Type: notify.NotifyType.Success
+      }
+    }
+  }, (error: boolean) => {
+    finish(error)
+  })
+}
+
+export const deleteScope = (target: couponscope.Scope, finish: (error: boolean) => void) => {
+  scope.deleteScope({
+    ID: target?.ID,
+    Message: {
+      Error: {
+        Title: 'MSG_DELETE_COUPON_SCOPE',
+        Message: 'MSG_DELETE_COUPON_SCOPE_FAIL',
+        Popup: true,
+        Type: notify.NotifyType.Error
+      },
+      Info: {
+        Title: 'MSG_DELETE_COUPON_SCOPE',
+        Message: 'MSG_DELETE_COUPON_SCOPE_SUCCESS',
+        Popup: true,
+        Type: notify.NotifyType.Success
+      }
+    }
+  }, (error: boolean) => {
+    finish(error)
+  })
+}
