0e021c857a5feca6f1d4cd1ea5cc8aec367b4fa9
lidaqiang <dqli161@163.com>
2023-10-16 10:51:59 +0800

    add getAppCoupons

--- sdk/coupon.ts
+++ sdk/coupon.ts
@@ -30,11 +30,38 @@ export const getCoupons = (pageStart: number, pages: number, done?: (error: bool
   getPageCoupons(pageStart, pages ? pageStart + pages : pages, done)
 }
 
+const getPageAppCoupons = (pageIndex: number, pageEnd: number, done?: (error: boolean, totalPages: number, totalRows: number) => void) => {
+  _coupon.getAppCoupons({
+    TargetAppID: AppID.value,
+    Offset: pageIndex * constant.DefaultPageSize,
+    Limit: constant.DefaultPageSize,
+    Message: {
+      Error: {
+        Title: 'MSG_GET_APP_DEFAULT_GOODS_FAIL',
+        Popup: true,
+        Type: notify.NotifyType.Error
+      }
+    }
+  }, (error: boolean, rows?: Array<coupon.Coupon>, total?: number) => {
+    if (error || !rows?.length || (pageEnd > 0 && pageIndex === pageEnd - 1)) {
+      const totalPages = Math.ceil(total as number / constant.DefaultPageSize)
+      done?.(error, totalPages, total as number)
+      return
+    }
+    getPageAppCoupons(++pageIndex, pageEnd, done)
+  })
+}
+
+export const getAppCoupons = (pageStart: number, pages: number, done?: (error: boolean, totalPages: number, totalRows: number) => void) => {
+  getPageAppCoupons(pageStart, pages ? pageStart + pages : pages, done)
+}
+
 export const _coupons = computed(() => _coupon.coupons(AppID.value))
 
 export const createCoupon = (target: coupon.Coupon, finish: (error: boolean) => void) => {
   _coupon.createCoupon({
     ...target,
+    TargetAppID: target.AppID,
     NotifyMessage: {
       Error: {
         Title: 'MSG_CREATE_COUPON',
@@ -57,6 +84,7 @@ export const createCoupon = (target: coupon.Coupon, finish: (error: boolean) => 
 export const updateCoupon = (target: coupon.Coupon, finish: (error: boolean) => void) => {
   _coupon.updateCoupon({
     ...target,
+    TargetAppID: target.AppID,
     NotifyMessage: {
       Error: {
         Title: 'MSG_UPDATE_COUPON',
