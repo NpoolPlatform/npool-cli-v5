d6ae2b5d11f881c0f0be4c3aa6999ffc9f656b55
lidaqiang <dqli161@163.com>
2023-11-03 14:57:04 +0800

    fix wrong path

--- inspire/coupon/app/scope/const.ts
+++ inspire/coupon/app/scope/const.ts
@@ -2,5 +2,5 @@ export enum API {
   CREATE_APPGOODSCOPE = '/inspire/v1/create/appgoodscope',
   DELETE_APPGOODSCOPE = '/inspire/v1/delete/appgoodscope',
   GET_APP_APPGOODSCOPES = '/inspire/v1/get/app/appgoodscope',
-  GET_APPGOODSCOPES = '/inspire/v1/get/appgoodscope',
+  GET_APPGOODSCOPES = '/inspire/v1/get/appgoodscopes',
 }
