d4e8936afd00630ebfb8532ecfcaf961c7138919
lidaqiang <dqli161@163.com>
2023-12-05 18:17:29 +0800

    add subscriber

---
+++ appuser/subscribe/const.ts
@@ -0,0 +1,5 @@
+export enum APIS {
+    CREATE_SUBSCRIBE = '/appuser/v1/create/subscribe',
+    GET_SUBSCRIBES = '/appuser/v1/get/subscribes',
+    DELETE_SUBSCRIBE = '/appuser/v1/delete/subscribe'
+}
