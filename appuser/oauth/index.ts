0cd3cff8a52258ea8039519522bf8ffa4305dfb1
lidaqiang <dqli161@163.com>
2023-12-05 17:00:42 +0800

    update third

---
+++ appuser/oauth/index.ts
@@ -0,0 +1,46 @@
+import { defineStore } from 'pinia'
+import { APIS as APIEnum } from './const'
+import { doActionWithError } from '../../request'
+import {
+  GetOAuthLoginURLRequest,
+  GetOAuthLoginURLResponse,
+  OAuthLoginRequest,
+  OAuthLoginResponse
+} from './types'
+import { User, useLocalUserStore } from '../user'
+
+export const useOAuthStore = defineStore('oauth', {
+  state: () => ({}),
+  getters: {
+  },
+  actions: {
+    getOAuthLoginURL (req: GetOAuthLoginURLRequest, done: (error: boolean, url?: string) => void) {
+      doActionWithError<GetOAuthLoginURLRequest, GetOAuthLoginURLResponse>(
+        APIEnum.GET_OAUTH_LOGIN_URL,
+        req,
+        req.Message,
+        (resp: GetOAuthLoginURLResponse): void => {
+          done(false, resp.Info)
+        }, () => {
+          done(true)
+        }
+      )
+    },
+    oauthLogin (req: OAuthLoginRequest, done: (error: boolean, user?: User) => void) {
+      doActionWithError<OAuthLoginRequest, OAuthLoginResponse>(
+        APIEnum.OAUTH_LOGIN,
+        req,
+        req.Message,
+        (resp: OAuthLoginResponse): void => {
+          const user = useLocalUserStore()
+          user.setUser(resp.Info)
+          done(false, resp.Info)
+        }, () => {
+          done(true)
+        }
+      )
+    }
+  }
+})
+
+export * from './types'
--- appuser/oauth/thirdparty/const.ts
+++ appuser/oauth/thirdparty/const.ts
@@ -2,5 +2,7 @@ export enum APIS {
   CREATE_OAUTH_THIRD_PARTY = '/appuser/v1/create/oauththirdparty',
   UPDATE_OAUTH_THIRD_PARTY = '/appuser/v1/update/oauththirdparty',
   GET_OAUTH_THIRD_PARTIES = '/appuser/v1/get/oauththirdparties',
-  DELETE_OAUTH_THIRD_PARTY = '/appuser/v1/delete/oauththirdparty'
+  DELETE_OAUTH_THIRD_PARTY = '/appuser/v1/delete/oauththirdparty',
+  GET_OAUTH_LOGIN_URL = '/appuser/v1/get/oauthloginurl',
+  OAUTH_LOGIN = '/appuser/v1/oauthlogin'
 }
