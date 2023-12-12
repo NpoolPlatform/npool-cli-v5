f5933fc2b788f727f2fa418ab6fd4348b2d8f846
lidaqiang <dqli161@163.com>
2023-12-12 13:32:13 +0800

    update commit id

--- appuser/user/local/index.ts
+++ appuser/user/local/index.ts
@@ -56,8 +56,11 @@ export const useLocalUserStore = defineStore('local-user', {
       console.log('user.EntID: ', user.EntID)
       console.log('user.LoginToken: ', user.LoginToken)
       if (user) {
+        console.log('****************')
         Cookies.set('X-User-ID', user.EntID, { expires: '4h', secure: true })
         Cookies.set('X-App-Login-Token', user.LoginToken, { expires: '4h', secure: true })
+        console.log('X-User-ID:', Cookies.get('X-User-ID'))
+        console.log('X-App-Login-Token:', Cookies.get('X-App-Login-Token'))
       }
     },
     restUser () {
