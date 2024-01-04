0802265781ba142c8ffb561fcd38a950f5240d99
lidaqiang <dqli161@163.com>
2024-01-04 14:29:09 +0800

    add cashcontrol api

---
+++ inspire/coupon/app/cashcontrol/types.ts
@@ -0,0 +1,61 @@
+import { BaseRequest } from '../../../../request'
+import { ControlType, CouponType } from '../../const'
+
+
+export interface CashControl {
+    ID: number
+    EntID: string
+    AppID: string
+    CouponID: string
+    CouponName: string
+    CouponTypeStr: string
+    CouponType: CouponType
+    CouponDenomination: string
+    ControlTypeStr: string
+    ControlType: ControlType
+    Value: string
+    CreatedAt: number
+    UpdatedAt: number
+}
+  
+export interface CreateCashControlRequest extends BaseRequest {
+    CouponID: string
+    ControlType: ControlType
+    Value?: string
+}
+  
+export interface CreateCashControlResponse {
+    Info: CashControl
+}
+  
+export interface DeleteCashControlRequest extends BaseRequest {
+    ID: number
+    EntID: string
+    TargetAppID: string
+}
+  
+export interface DeleteCashControlResponse {
+    Info: CashControl
+}
+  
+export interface GetCashControlsRequest extends BaseRequest {
+    TargetAppID: string
+    Offset: number
+    Limit: number
+}
+  
+export interface GetCashControlsResponse {
+    Infos: CashControl[]
+    Total: number
+}
+
+export interface GetAppCashControlsRequest extends BaseRequest {
+    AppID?: string
+    Offset: number
+    Limit: number
+  }
+  
+export interface GetAppCashControlsResponse {
+    Infos: CashControl[]
+    Total: number
+}
