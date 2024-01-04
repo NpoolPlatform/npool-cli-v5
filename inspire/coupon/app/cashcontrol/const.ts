3fdc5951c7d01d605331941e6610342e635742e6
lidaqiang <dqli161@163.com>
2024-01-04 14:29:09 +0800

    add cashcontrol api

---
+++ inspire/coupon/app/cashcontrol/const.ts
@@ -0,0 +1,6 @@
+export enum API {
+  CREATE_CASHCONTROL = '/inspire/v1/create/cashcontrol',
+  DELETE_CASHCONTROL = '/inspire/v1/delete/cashcontrol',
+  GET_APP_CASHCONTROLS = '/inspire/v1/get/app/cashcontrol',
+  GET_CASHCONTROLS = '/inspire/v1/get/cashcontrols',
+}
