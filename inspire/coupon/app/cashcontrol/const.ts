1df6326138cdbb49d081c2b83c021ea83255a228
lidaqiang <dqli161@163.com>
2024-01-09 16:01:32 +0800

    change paht

--- inspire/coupon/app/cashcontrol/const.ts
+++ inspire/coupon/app/cashcontrol/const.ts
@@ -1,6 +1,6 @@
 export enum API {
   CREATE_CASHCONTROL = '/inspire/v1/create/cashcontrol',
   DELETE_CASHCONTROL = '/inspire/v1/delete/cashcontrol',
-  GET_APP_CASHCONTROLS = '/inspire/v1/get/app/cashcontrol',
+  GET_APP_CASHCONTROLS = '/inspire/v1/get/app/cashcontrols',
   GET_CASHCONTROLS = '/inspire/v1/get/cashcontrols',
 }
