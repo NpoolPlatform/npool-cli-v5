a720a4bfee65a263ac5e8314a9b47f3f7c461561
Zhao KK <kikakkz@hotmail.com>
2024-01-16 09:35:49 +0800

    Correct app goods page

--- sdk/appgood.ts
+++ sdk/appgood.ts
@@ -4,59 +4,84 @@ import { AppID } from './localapp'
 
 const _appgood = appgood.useAppGoodStore()
 
-const getPageAppGoods = (pageIndex: number, pageEnd: number, done?: (error: boolean, totalPages: number, totalRows: number) => void) => {
+const getPageAppGoods = (offset: number, limit: number, pageIndex: number, done?: (error: boolean, fetchedRows: number, totalRows: number) => void) => {
+  const reqOffset = offset + pageIndex * constant.DefaultPageSize
+  let reqLimit = constant.DefaultPageSize
+  if (limit > 0) {
+    reqLimit = limit - pageIndex * constant.DefaultPageSize
+  }
   _appgood.getAppGoods({
-    Offset: pageIndex * constant.DefaultPageSize,
-    Limit: constant.DefaultPageSize,
+    Offset: reqOffset,
+    Limit: reqLimit,
     Message: {
       Error: {
-        Title: 'MSG_GET_GOODS',
-        Message: 'MSG_GET_GOODS_FAIL',
+        Title: 'MSG_GET_APP_COUNTRIES',
+        Message: 'MSG_GET_APP_COUNTRIES_FAIL',
         Popup: true,
         Type: notify.NotifyType.Error
       }
     }
-  }, (error: boolean, goods?: Array<appgood.Good>, total?: number) => {
-    if (error || !goods?.length || (pageEnd > 0 && pageIndex === pageEnd - 1)) {
-      const totalPages = Math.ceil(total as number / constant.DefaultPageSize)
-      done?.(error, totalPages, total as number)
+  }, (error: boolean, rows?: Array<appgood.Good>, totalRows?: number) => {
+    if (error || !rows?.length) {
+      if (limit === 0) {
+        limit = totalRows as number
+      } else {
+        limit = Math.max(limit - (pageIndex + 1) * constant.DefaultPageSize)
+      }
+      done?.(error, limit, totalRows as number)
       return
     }
-    getPageAppGoods(++pageIndex, pageEnd, done)
+    if (limit <= pageIndex * constant.DefaultPageSize && limit > 0) {
+      done?.(error, totalRows as number - offset, 0)
+      return
+    }
+    getPageAppGoods(offset, limit, ++pageIndex, done)
   })
 }
 
-export const getAppGoods = (pageStart: number, pages: number, done?: (error: boolean, totalPages: number, totalRows: number) => void) => {
-  getPageAppGoods(pageStart, pages ? pageStart + pages : pages, done)
+export const getAppGoods = (offset: number, limit: number, done?: (error: boolean, fetchedRows: number, totalRows: number) => void) => {
+  getPageAppGoods(offset, limit, 0, done)
 }
 
-const getNPageAppGoods = (pageIndex: number, pageEnd: number, done?: (error: boolean, totalPages: number, totalRows: number) => void) => {
+const getNPageAppGoods = (offset: number, limit: number, pageIndex: number, done?: (error: boolean, fetchedRows: number, totalRows: number) => void) => {
+  const reqOffset = offset + pageIndex * constant.DefaultPageSize
+  let reqLimit = constant.DefaultPageSize
+  if (limit > 0) {
+    reqLimit = limit - pageIndex * constant.DefaultPageSize
+  }
   _appgood.getNAppGoods({
     TargetAppID: AppID.value,
-    Offset: pageIndex * constant.DefaultPageSize,
-    Limit: constant.DefaultPageSize,
+    Offset: reqOffset,
+    Limit: reqLimit,
     Message: {
       Error: {
-        Title: 'MSG_GET_GOODS',
-        Message: 'MSG_GET_GOODS_FAIL',
+        Title: 'MSG_GET_APP_COUNTRIES',
+        Message: 'MSG_GET_APP_COUNTRIES_FAIL',
         Popup: true,
         Type: notify.NotifyType.Error
       }
     }
-  }, (error: boolean, orders?: Array<appgood.Good>, total?: number) => {
-    if (error || !orders?.length || (pageEnd > 0 && pageIndex === pageEnd - 1)) {
-      const totalPages = Math.ceil(total as number / constant.DefaultPageSize)
-      done?.(error, totalPages, total as number)
+  }, (error: boolean, rows?: Array<appgood.Good>, totalRows?: number) => {
+    if (error || !rows?.length) {
+      if (limit === 0) {
+        limit = totalRows as number
+      } else {
+        limit = Math.max(limit - (pageIndex + 1) * constant.DefaultPageSize)
+      }
+      done?.(error, limit, totalRows as number)
       return
     }
-    getNPageAppGoods(++pageIndex, pageEnd, done)
+    if (limit <= pageIndex * constant.DefaultPageSize && limit > 0) {
+      done?.(error, totalRows as number - offset, 0)
+      return
+    }
+    getPageAppGoods(offset, limit, ++pageIndex, done)
   })
 }
 
-export const getNAppGoods = (pageStart: number, pages: number, done?: (error: boolean, totalPages: number, totalRows: number) => void) => {
-  getNPageAppGoods(pageStart, pages ? pageStart + pages : pages, done)
+export const getNAppGoods = (offset: number, limit: number, done?: (error: boolean, totalPages: number, totalRows: number) => void) => {
+  getNPageAppGoods(offset, limit, 0, done)
 }
 
 export const appGoods = computed(() => _appgood.goods(AppID.value))
-
 export const appGoodCancelable = (id: string) => _appgood.cancelable(AppID.value, id)
