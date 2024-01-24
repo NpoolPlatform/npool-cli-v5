dc8d207d032667b40378a90600281621ed8abdb0
lidaqiang <dqli161@163.com>
2024-01-24 11:53:32 +0800

    delete semicolon

--- inspire/event/schema.ts
+++ inspire/event/schema.ts
@@ -10,96 +10,96 @@
  */
 
 export interface Inspiregatewayeventv1Coupon {
-  ID?: string;
-  CouponType?: V1CouponType;
-  Value?: string;
-  Name?: string;
+  ID?: string
+  CouponType?: V1CouponType
+  Value?: string
+  Name?: string
 }
 
 export interface Inspiregatewayeventv1CreateEventRequest {
-  AppID?: string;
-  EventType?: V1UsedFor;
-  Coupons?: Inspiremanagereventv1Coupon[];
-  Credits?: string;
-  CreditsPerUSD?: string;
+  AppID?: string
+  EventType?: V1UsedFor
+  Coupons?: Inspiremanagereventv1Coupon[]
+  Credits?: string
+  CreditsPerUSD?: string
   /** @format int64 */
-  MaxConsecutive?: number;
-  GoodID?: string;
+  MaxConsecutive?: number
+  GoodID?: string
   /** @format int64 */
-  InviterLayers?: number;
+  InviterLayers?: number
 }
 
 export interface Inspiregatewayeventv1CreateEventResponse {
-  Info?: Inspiregatewayeventv1Event;
+  Info?: Inspiregatewayeventv1Event
 }
 
 export interface Inspiregatewayeventv1Event {
-  ID?: string;
-  AppID?: string;
-  AppName?: string;
-  EventType?: V1UsedFor;
-  Coupons?: Inspiregatewayeventv1Coupon[];
-  Credits?: string;
-  CreditsPerUSD?: string;
+  ID?: string
+  AppID?: string
+  AppName?: string
+  EventType?: V1UsedFor
+  Coupons?: Inspiregatewayeventv1Coupon[]
+  Credits?: string
+  CreditsPerUSD?: string
   /** @format int64 */
-  MaxConsecutive?: number;
-  GoodID?: string;
-  GoodName?: string;
+  MaxConsecutive?: number
+  GoodID?: string
+  GoodName?: string
   /** @format int64 */
-  InviterLayers?: number;
+  InviterLayers?: number
   /** @format int64 */
-  CreatedAt?: number;
+  CreatedAt?: number
   /** @format int64 */
-  UpdatedAt?: number;
+  UpdatedAt?: number
   /** @format int64 */
-  DeletedAt?: number;
+  DeletedAt?: number
 }
 
 export interface Inspiregatewayeventv1GetEventsRequest {
-  AppID?: string;
+  AppID?: string
   /** @format int32 */
-  Offset?: number;
+  Offset?: number
   /** @format int32 */
-  Limit?: number;
+  Limit?: number
 }
 
 export interface Inspiregatewayeventv1GetEventsResponse {
-  Infos?: Inspiregatewayeventv1Event[];
+  Infos?: Inspiregatewayeventv1Event[]
   /** @format int64 */
-  Total?: number;
+  Total?: number
 }
 
 export interface Inspiregatewayeventv1UpdateEventRequest {
-  ID?: string;
-  AppID?: string;
-  Coupons?: Inspiremanagereventv1Coupon[];
-  Credits?: string;
-  CreditsPerUSD?: string;
+  ID?: string
+  AppID?: string
+  Coupons?: Inspiremanagereventv1Coupon[]
+  Credits?: string
+  CreditsPerUSD?: string
   /** @format int64 */
-  MaxConsecutive?: number;
+  MaxConsecutive?: number
   /** @format int64 */
-  InviterLayers?: number;
+  InviterLayers?: number
 }
 
 export interface Inspiregatewayeventv1UpdateEventResponse {
-  Info?: Inspiregatewayeventv1Event;
+  Info?: Inspiregatewayeventv1Event
 }
 
 export interface Inspiremanagereventv1Coupon {
-  ID?: string;
-  CouponType?: V1CouponType;
+  ID?: string
+  CouponType?: V1CouponType
 }
 
 export interface ProtobufAny {
-  "@type"?: string;
-  [key: string]: any;
+  "@type"?: string
+  [key: string]: any
 }
 
 export interface RpcStatus {
   /** @format int32 */
-  code?: number;
-  message?: string;
-  details?: ProtobufAny[];
+  code?: number
+  message?: string
+  details?: ProtobufAny[]
 }
 
 /** @default "DefaultCouponType" */
@@ -139,43 +139,43 @@ export enum V1UsedFor {
   Announcement = "Announcement",
 }
 
-export type QueryParamsType = Record<string | number, any>;
-export type ResponseFormat = keyof Omit<Body, "body" | "bodyUsed">;
+export type QueryParamsType = Record<string | number, any>
+export type ResponseFormat = keyof Omit<Body, "body" | "bodyUsed">
 
 export interface FullRequestParams extends Omit<RequestInit, "body"> {
   /** set parameter to `true` for call `securityWorker` for this request */
-  secure?: boolean;
+  secure?: boolean
   /** request path */
-  path: string;
+  path: string
   /** content type of request body */
-  type?: ContentType;
+  type?: ContentType
   /** query params */
-  query?: QueryParamsType;
+  query?: QueryParamsType
   /** format of response (i.e. response.json() -> format: "json") */
-  format?: ResponseFormat;
+  format?: ResponseFormat
   /** request body */
-  body?: unknown;
+  body?: unknown
   /** base url */
-  baseUrl?: string;
+  baseUrl?: string
   /** request cancellation token */
-  cancelToken?: CancelToken;
+  cancelToken?: CancelToken
 }
 
-export type RequestParams = Omit<FullRequestParams, "body" | "method" | "query" | "path">;
+export type RequestParams = Omit<FullRequestParams, "body" | "method" | "query" | "path">
 
 export interface ApiConfig<SecurityDataType = unknown> {
-  baseUrl?: string;
-  baseApiParams?: Omit<RequestParams, "baseUrl" | "cancelToken" | "signal">;
-  securityWorker?: (securityData: SecurityDataType | null) => Promise<RequestParams | void> | RequestParams | void;
-  customFetch?: typeof fetch;
+  baseUrl?: string
+  baseApiParams?: Omit<RequestParams, "baseUrl" | "cancelToken" | "signal">
+  securityWorker?: (securityData: SecurityDataType | null) => Promise<RequestParams | void> | RequestParams | void
+  customFetch?: typeof fetch
 }
 
 export interface HttpResponse<D extends unknown, E extends unknown = unknown> extends Response {
-  data: D;
-  error: E;
+  data: D
+  error: E
 }
 
-type CancelToken = Symbol | string | number;
+type CancelToken = Symbol | string | number
 
 export enum ContentType {
   Json = "application/json",
@@ -185,52 +185,52 @@ export enum ContentType {
 }
 
 export class HttpClient<SecurityDataType = unknown> {
-  public baseUrl: string = "";
-  private securityData: SecurityDataType | null = null;
-  private securityWorker?: ApiConfig<SecurityDataType>["securityWorker"];
-  private abortControllers = new Map<CancelToken, AbortController>();
-  private customFetch = (...fetchParams: Parameters<typeof fetch>) => fetch(...fetchParams);
+  public baseUrl: string = ""
+  private securityData: SecurityDataType | null = null
+  private securityWorker?: ApiConfig<SecurityDataType>["securityWorker"]
+  private abortControllers = new Map<CancelToken, AbortController>()
+  private customFetch = (...fetchParams: Parameters<typeof fetch>) => fetch(...fetchParams)
 
   private baseApiParams: RequestParams = {
     credentials: "same-origin",
     headers: {},
     redirect: "follow",
     referrerPolicy: "no-referrer",
-  };
+  }
 
   constructor(apiConfig: ApiConfig<SecurityDataType> = {}) {
-    Object.assign(this, apiConfig);
+    Object.assign(this, apiConfig)
   }
 
   public setSecurityData = (data: SecurityDataType | null) => {
-    this.securityData = data;
-  };
+    this.securityData = data
+  }
 
   protected encodeQueryParam(key: string, value: any) {
-    const encodedKey = encodeURIComponent(key);
-    return `${encodedKey}=${encodeURIComponent(typeof value === "number" ? value : `${value}`)}`;
+    const encodedKey = encodeURIComponent(key)
+    return `${encodedKey}=${encodeURIComponent(typeof value === "number" ? value : `${value}`)}`
   }
 
   protected addQueryParam(query: QueryParamsType, key: string) {
-    return this.encodeQueryParam(key, query[key]);
+    return this.encodeQueryParam(key, query[key])
   }
 
   protected addArrayQueryParam(query: QueryParamsType, key: string) {
-    const value = query[key];
-    return value.map((v: any) => this.encodeQueryParam(key, v)).join("&");
+    const value = query[key]
+    return value.map((v: any) => this.encodeQueryParam(key, v)).join("&")
   }
 
   protected toQueryString(rawQuery?: QueryParamsType): string {
-    const query = rawQuery || {};
-    const keys = Object.keys(query).filter((key) => "undefined" !== typeof query[key]);
+    const query = rawQuery || {}
+    const keys = Object.keys(query).filter((key) => "undefined" !== typeof query[key])
     return keys
       .map((key) => (Array.isArray(query[key]) ? this.addArrayQueryParam(query, key) : this.addQueryParam(query, key)))
-      .join("&");
+      .join("&")
   }
 
   protected addQueryParams(rawQuery?: QueryParamsType): string {
-    const queryString = this.toQueryString(rawQuery);
-    return queryString ? `?${queryString}` : "";
+    const queryString = this.toQueryString(rawQuery)
+    return queryString ? `?${queryString}` : ""
   }
 
   private contentFormatters: Record<ContentType, (input: any) => any> = {
@@ -239,7 +239,7 @@ export class HttpClient<SecurityDataType = unknown> {
     [ContentType.Text]: (input: any) => (input !== null && typeof input !== "string" ? JSON.stringify(input) : input),
     [ContentType.FormData]: (input: any) =>
       Object.keys(input || {}).reduce((formData, key) => {
-        const property = input[key];
+        const property = input[key]
         formData.append(
           key,
           property instanceof Blob
@@ -247,11 +247,11 @@ export class HttpClient<SecurityDataType = unknown> {
             : typeof property === "object" && property !== null
             ? JSON.stringify(property)
             : `${property}`,
-        );
-        return formData;
+        )
+        return formData
       }, new FormData()),
     [ContentType.UrlEncoded]: (input: any) => this.toQueryString(input),
-  };
+  }
 
   protected mergeRequestParams(params1: RequestParams, params2?: RequestParams): RequestParams {
     return {
@@ -263,31 +263,31 @@ export class HttpClient<SecurityDataType = unknown> {
         ...(params1.headers || {}),
         ...((params2 && params2.headers) || {}),
       },
-    };
+    }
   }
 
   protected createAbortSignal = (cancelToken: CancelToken): AbortSignal | undefined => {
     if (this.abortControllers.has(cancelToken)) {
-      const abortController = this.abortControllers.get(cancelToken);
+      const abortController = this.abortControllers.get(cancelToken)
       if (abortController) {
-        return abortController.signal;
+        return abortController.signal
       }
-      return void 0;
+      return void 0
     }
 
-    const abortController = new AbortController();
-    this.abortControllers.set(cancelToken, abortController);
-    return abortController.signal;
-  };
+    const abortController = new AbortController()
+    this.abortControllers.set(cancelToken, abortController)
+    return abortController.signal
+  }
 
   public abortRequest = (cancelToken: CancelToken) => {
-    const abortController = this.abortControllers.get(cancelToken);
+    const abortController = this.abortControllers.get(cancelToken)
 
     if (abortController) {
-      abortController.abort();
-      this.abortControllers.delete(cancelToken);
+      abortController.abort()
+      this.abortControllers.delete(cancelToken)
     }
-  };
+  }
 
   public request = async <T = any, E = any>({
     body,
@@ -304,11 +304,11 @@ export class HttpClient<SecurityDataType = unknown> {
       ((typeof secure === "boolean" ? secure : this.baseApiParams.secure) &&
         this.securityWorker &&
         (await this.securityWorker(this.securityData))) ||
-      {};
-    const requestParams = this.mergeRequestParams(params, secureParams);
-    const queryString = query && this.toQueryString(query);
-    const payloadFormatter = this.contentFormatters[type || ContentType.Json];
-    const responseFormat = format || requestParams.format;
+      {}
+    const requestParams = this.mergeRequestParams(params, secureParams)
+    const queryString = query && this.toQueryString(query)
+    const payloadFormatter = this.contentFormatters[type || ContentType.Json]
+    const responseFormat = format || requestParams.format
 
     return this.customFetch(`${baseUrl || this.baseUrl || ""}${path}${queryString ? `?${queryString}` : ""}`, {
       ...requestParams,
@@ -319,34 +319,34 @@ export class HttpClient<SecurityDataType = unknown> {
       signal: cancelToken ? this.createAbortSignal(cancelToken) : requestParams.signal,
       body: typeof body === "undefined" || body === null ? null : payloadFormatter(body),
     }).then(async (response) => {
-      const r = response as HttpResponse<T, E>;
-      r.data = null as unknown as T;
-      r.error = null as unknown as E;
+      const r = response as HttpResponse<T, E>
+      r.data = null as unknown as T
+      r.error = null as unknown as E
 
       const data = !responseFormat
         ? r
         : await response[responseFormat]()
             .then((data) => {
               if (r.ok) {
-                r.data = data;
+                r.data = data
               } else {
-                r.error = data;
+                r.error = data
               }
-              return r;
+              return r
             })
             .catch((e) => {
-              r.error = e;
-              return r;
-            });
+              r.error = e
+              return r
+            })
 
       if (cancelToken) {
-        this.abortControllers.delete(cancelToken);
+        this.abortControllers.delete(cancelToken)
       }
 
-      if (!response.ok) throw data;
-      return data;
-    });
-  };
+      if (!response.ok) throw data
+      return data
+    })
+  }
 }
 
 /**
@@ -405,5 +405,5 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
         format: "json",
         ...params,
       }),
-  };
+  }
 }
