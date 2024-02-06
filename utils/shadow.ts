12fe64cf1c70354e44b74a286b94f20126b80261
Zhao KK <kikakkz@hotmail.com>
2024-02-06 15:20:21 +0800

    Add order payment page

--- utils/shadow.ts
+++ utils/shadow.ts
@@ -1,20 +1,29 @@
 import { SignMethodType } from '../appuser/base'
 
 export const shadowEntID = (entID: string) => {
+  if (!entID) {
+    return ''
+  }
   return entID.slice(0, 5) + '***-*****-***' + entID.slice(-6, -1)
 }
 
 export const shortEntID = (entID: string) => {
+  if (!entID) {
+    return ''
+  }
   return entID.slice(-12, -1)
 }
 
 export const shadowAccount = (account: string, accountType: SignMethodType) => {
+  if (!account) {
+    return ''
+  }
   let suffix = ''
   let prefix = ''
   if (accountType === SignMethodType.Email) {
     suffix = account.split('@')[1]
   }
-  [...account.split('@')[0]].forEach((c, i) => {
+  [...account?.split('@')[0]].forEach((c, i) => {
     prefix += i <= 3 ? c : '*'
   })
   return prefix + '@' + suffix
