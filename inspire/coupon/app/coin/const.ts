88e013237f4d96de27fc4b13cac1f857a69a3cb0
lidaqiang <dqli161@163.com>
2024-03-05 18:49:04 +0800

    delete couponcoin

--- inspire/coupon/app/coin/const.ts
+++
@@ -1,6 +0,0 @@
-export enum API {
-  CREATE_COUPONCOIN = '/inspire/v1/create/couponcoin',
-  DELETE_COUPONCOIN = '/inspire/v1/delete/couponcoin',
-  GET_APP_COUPONCOINS = '/inspire/v1/get/app/couponcoins',
-  GET_COUPONCOINS = '/inspire/v1/get/couponcoins',
-}
