360bed3719882d7e749d05288f0d5d1ce41b197c
lidaqiang <dqli161@163.com>
2024-03-05 15:11:29 +0800

    add util method

--- const/const.ts
+++ const/const.ts
@@ -1,6 +1,7 @@
 const NotSet = 'NOT SET'
 
 const VerificationCodeLength = 6
+const RecoveryCodeLength = 11
 const MinPasswordLength = 8
 const MaxPasswordLength = 32
 const SecondsEachDay = 24 * 60 * 60
@@ -19,6 +20,7 @@ export const DefaultPageSize = 100
 export {
   NotSet,
   VerificationCodeLength,
+  RecoveryCodeLength,
   MinPasswordLength,
   MaxPasswordLength,
   InvalidID,
