338e581d420562ff342d43e663f93d39b3187bf2
Zhao KK <kikakkz@hotmail.com>
2024-06-18 10:36:05 +0800

    Support topmost poster

--- good/app/good/topmost/const.ts
+++ good/app/good/topmost/const.ts
@@ -1,10 +1,11 @@
 export enum API {
-    GET_TOPMOST = '/good/v1/get/topmost',
     CREATE_TOPMOST = '/good/v1/create/topmost',
-    CREATE_N_TOPMOST = '/good/v1/create/n/topmost',
-    GET_TOPMOSTS = '/good/v1/get/topmosts',
-    GET_N_TOPMOSTS = '/good/v1/get/n/topmosts',
     UPDATE_TOPMOST = '/good/v1/update/topmost',
-    UPDATE_N_TOPMOST = '/good/v1/update/n/topmost',
+    GET_TOPMOSTS = '/good/v1/get/topmosts',
     DELETE_TOPMOST = '/good/v1/delete/topmost',
+
+    ADMIN_CREATE_TOPMOST = '/good/v1/admin/create/topmost',
+    ADMIN_UPDATE_TOPMOST = '/good/v1/admin/update/topmost',
+    ADMIN_GET_TOPMOSTS = '/good/v1/admin/get/topmosts',
+    ADMIN_DELETE_TOPMOST = '/good/v1/admin/delete/topmost'
 }
---
+++ good/app/good/topmost/poster/types.ts
@@ -0,0 +1,106 @@
+import { BaseRequest } from '../../../../../request'
+import { GoodTopMostType } from '../../../../base'
+
+export interface PosterInfo {
+    Poster: string
+    Index: number
+}
+
+export interface Poster extends PosterInfo {
+    ID: number
+    EntID: string
+    AppID: string
+    AppName: string
+    TopMostID: string
+    TopMostType: GoodTopMostType
+    TopMostTitle: string
+    TopMostMessage: string
+    TopMostTargetUrl: string
+    CreatedAt: number
+    UpdatedAt: number
+}
+
+export interface CreatePosterRequest extends BaseRequest {
+  TopMostID: string
+  Poster: string
+  Index?: number
+}
+
+export interface CreatePosterResponse {
+  Info: Poster
+}
+
+export interface UpdatePosterRequest extends BaseRequest {
+  ID: number
+  EntID: string
+  Poster?: string
+  Index?: number
+}
+
+export interface UpdatePosterResponse {
+  Info: Poster
+}
+
+export interface GetPostersRequest extends BaseRequest {
+  TopMostID?: string
+  Offset: number
+  Limit: number
+}
+
+export interface GetPostersResponse {
+  Infos: Poster[]
+  Total: number
+}
+
+export interface DeletePosterRequest extends BaseRequest {
+  ID: number
+  EntID: string
+}
+
+export interface DeletePosterResponse {
+  Info: Poster
+}
+
+export interface AdminCreatePosterRequest extends BaseRequest {
+  TargetAppID: string
+  TopMostID: string
+  Poster: string
+  Index?: number
+}
+
+export interface AdminCreatePosterResponse {
+  Info: Poster
+}
+
+export interface AdminUpdatePosterRequest extends BaseRequest {
+  ID: number
+  EntID: string
+  TargetAppID: string
+  Poster?: string
+  Index?: number
+}
+
+export interface AdminUpdatePosterResponse {
+  Info: Poster
+}
+
+export interface AdminGetPostersRequest extends BaseRequest {
+  TargetAppID: string
+  Offset: number
+  Limit: number
+}
+
+export interface AdminGetPostersResponse {
+  Infos: Poster[]
+  Total: number
+}
+
+export interface AdminDeletePosterRequest extends BaseRequest {
+  ID: number
+  EntID: string
+  TargetAppID: string
+}
+
+export interface AdminDeletePosterResponse {
+  Info: Poster
+}
