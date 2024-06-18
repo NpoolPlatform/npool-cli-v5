6cbcb27fda0c2e3b69bfee605ab1bff02254b8b9
Zhao KK <kikakkz@hotmail.com>
2024-06-18 10:36:05 +0800

    Support topmost poster

---
+++ good/app/good/topmost/poster/const.ts
@@ -0,0 +1,10 @@
+export enum API {
+  CREATE_TOPMOST_POSTER = '/good/v1/create/topmostposter',
+  UPDATE_TOPMOST_POSTER = '/good/v1/update/topmostposter',
+  GET_TOPMOST_POSTERS = '/good/v1/get/topmostposters',
+  DELETE_TOPMOST_POSTER = '/good/v1/delete/topmostposter',
+  ADMIN_CREATE_TOPMOST_POSTER = '/good/v1/admin/create/topmostposter',
+  ADMIN_UPDATE_TOPMOST_POSTER = '/good/v1/admin/update/topmostposter',
+  ADMIN_GET_TOPMOST_POSTERS = '/good/v1/admin/get/topmostposters',
+  ADMIN_DELETE_TOPMOST_POSTER = '/good/v1/admin/delete/topmostposter'
+}
