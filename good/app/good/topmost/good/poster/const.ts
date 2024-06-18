b5a4c34b71687cd05ad9ba6af376ec4445b5b3c7
Zhao KK <kikakkz@hotmail.com>
2024-06-18 11:00:01 +0800

    Add topmost good poster

---
+++ good/app/good/topmost/good/poster/const.ts
@@ -0,0 +1,10 @@
+export enum API {
+  CREATE_TOPMOST_GOOD_POSTER = '/good/v1/create/topmostgoodposter',
+  UPDATE_TOPMOST_GOOD_POSTER = '/good/v1/update/topmostgoodposter',
+  GET_TOPMOST_GOOD_POSTERS = '/good/v1/get/topmostgoodposters',
+  DELETE_TOPMOST_GOOD_POSTER = '/good/v1/delete/topmostgoodposter',
+  ADMIN_CREATE_TOPMOST_GOOD_POSTER = '/good/v1/admin/create/topmostgoodposter',
+  ADMIN_UPDATE_TOPMOST_GOOD_POSTER = '/good/v1/admin/update/topmostgoodposter',
+  ADMIN_GET_TOPMOST_GOOD_POSTERS = '/good/v1/admin/get/topmostgoodposters',
+  ADMIN_DELETE_TOPMOST_GOOD_POSTER = '/good/v1/admin/delete/topmostgoodposter'
+}
