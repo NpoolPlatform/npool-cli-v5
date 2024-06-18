7a5996bd296d34f173d7cee653f83d538ec2fdfc
Zhao KK <kikakkz@hotmail.com>
2024-06-18 13:23:25 +0800

    Add order out of gas apis

---
+++ order/outofgas/const.ts
@@ -0,0 +1,5 @@
+export enum API {
+    GET_OUTOFGASES = '/order/v1/get/outofgases',
+    GET_MY_OUTOFGASES = '/order/v1/get/my/outofgases',
+    ADMIN_GET_OUTOFGASES = '/order/v1/admin/get/outofgases'
+}
