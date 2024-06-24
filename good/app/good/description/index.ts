8e70aa214bc5f8b9c1ab660cf63099fb4560ac68
Zhao KK <kikakkz@hotmail.com>
2024-06-24 15:43:02 +0800

    Add good description

--- good/app/good/description/index.ts
+++ good/app/good/description/index.ts
@@ -22,18 +22,18 @@ import {
 } from './types'
 import { formalizeAppID } from '../../../../appuser/app/local'
 
-export const useDescriptionStore = defineStore('app-good-descriptions', {
+export const useDescriptionStore = defineStore('appGoodDescriptions', {
   state: () => ({
     Descriptions: new Map<string, Array<Description>>()
   }),
   getters: {
-    appFee (): (appID: string | undefined, id: string) => Description | undefined {
+    description (): (appID: string | undefined, id: string) => Description | undefined {
       return (appID: string | undefined, id: string) => {
         appID = formalizeAppID(appID)
         return this.Descriptions.get(appID)?.find((el: Description) => el.EntID === id)
       }
     },
-    goods (): (appID?: string) => Array<Description> {
+    descriptions (): (appID?: string) => Array<Description> {
       return (appID?: string) => {
         appID = formalizeAppID(appID)
         return this.Descriptions.get(appID) || []
@@ -41,123 +41,123 @@ export const useDescriptionStore = defineStore('app-good-descriptions', {
     }
   },
   actions: {
-    addDescriptions (appID: string | undefined, goods: Array<Description>) {
+    addDescriptions (appID: string | undefined, descriptions: Array<Description>) {
       appID = formalizeAppID(appID)
-      let _goods = this.Descriptions.get(appID) as Array<Description>
-      if (!_goods) {
-        _goods = []
+      let _descriptions = this.Descriptions.get(appID) as Array<Description>
+      if (!_descriptions) {
+        _descriptions = []
       }
-      goods.forEach((good) => {
-        if (!good) return
-        const index = _goods.findIndex((el) => el.EntID === good.EntID)
-        _goods.splice(index >= 0 ? index : 0, index >= 0 ? 1 : 0, good)
+      descriptions.forEach((description) => {
+        if (!description) return
+        const index = _descriptions.findIndex((el) => el.EntID === description.EntID)
+        _descriptions.splice(index >= 0 ? index : 0, index >= 0 ? 1 : 0, description)
       })
-      this.Descriptions.set(appID, _goods)
+      this.Descriptions.set(appID, _descriptions)
     },
-    _deleteDescription (appID: string | undefined, good: Description) {
+    _deleteDescription (appID: string | undefined, description: Description) {
       appID = formalizeAppID(appID)
-      let _goods = this.Descriptions.get(appID) as Array<Description>
-      if (!_goods) {
-        _goods = []
+      let _descriptions = this.Descriptions.get(appID) as Array<Description>
+      if (!_descriptions) {
+        _descriptions = []
       }
-      const index = _goods.findIndex((el) => el.EntID === good.EntID)
-      _goods.splice(index >= 0 ? index : 0, index >= 0 ? 1 : 0)
-      this.Descriptions.set(appID, _goods)
+      const index = _descriptions.findIndex((el) => el.EntID === description.EntID)
+      _descriptions.splice(index >= 0 ? index : 0, index >= 0 ? 1 : 0)
+      this.Descriptions.set(appID, _descriptions)
     },
-    getDescriptions (req: GetDescriptionsRequest, done: (error: boolean, rows?: Array<Description>, total?: number) => void) {
+    getDescriptions (req: GetDescriptionsRequest, done?: (error: boolean, rows?: Array<Description>, total?: number) => void) {
       doActionWithError<GetDescriptionsRequest, GetDescriptionsResponse>(
         API.GET_GOOD_DESCRIPTIONS,
         req,
         req.Message,
         (resp: GetDescriptionsResponse): void => {
           this.addDescriptions(undefined, resp.Infos)
-          done(false, resp.Infos, resp.Total)
+          done?.(false, resp.Infos, resp.Total)
         }, () => {
-          done(true)
+          done?.(true)
         })
     },
-    createDescriptions (req: CreateDescriptionRequest, done: (error: boolean, row?: Description) => void) {
+    createDescription (req: CreateDescriptionRequest, done?: (error: boolean, row?: Description) => void) {
       doActionWithError<CreateDescriptionRequest, CreateDescriptionResponse>(
         API.CREATE_GOOD_DESCRIPTION,
         req,
         req.Message,
         (resp: CreateDescriptionResponse): void => {
           this.addDescriptions(undefined, [resp.Info])
-          done(false, resp.Info)
+          done?.(false, resp.Info)
         }, () => {
-          done(true)
+          done?.(true)
         })
     },
-    updateDescription (req: UpdateDescriptionRequest, done: (error: boolean, row?: Description) => void) {
+    updateDescription (req: UpdateDescriptionRequest, done?: (error: boolean, row?: Description) => void) {
       doActionWithError<UpdateDescriptionRequest, UpdateDescriptionResponse>(
         API.UPDATE_GOOD_DESCRIPTION,
         req,
         req.Message,
         (resp: UpdateDescriptionResponse): void => {
           this.addDescriptions(undefined, [resp.Info])
-          done(false, resp.Info)
+          done?.(false, resp.Info)
         }, () => {
-          done(true)
+          done?.(true)
         })
     },
-    deleteDescription (req: DeleteDescriptionRequest, done: (error: boolean, row?: Description) => void) {
+    deleteDescription (req: DeleteDescriptionRequest, done?: (error: boolean, row?: Description) => void) {
       doActionWithError<DeleteDescriptionRequest, DeleteDescriptionResponse>(
         API.DELETE_GOOD_DESCRIPTION,
         req,
         req.Message,
         (resp: DeleteDescriptionResponse): void => {
           this._deleteDescription(undefined, resp.Info)
-          done(false, resp.Info)
+          done?.(false, resp.Info)
         }, () => {
-          done(true)
+          done?.(true)
         })
     },
-    adminCreateDescriptions (req: AdminCreateDescriptionRequest, done: (error: boolean, row?: Description) => void) {
+    adminCreateDescription (req: AdminCreateDescriptionRequest, done?: (error: boolean, row?: Description) => void) {
       doActionWithError<AdminCreateDescriptionRequest, AdminCreateDescriptionResponse>(
         API.ADMIN_CREATE_GOOD_DESCRIPTION,
         req,
         req.Message,
         (resp: AdminCreateDescriptionResponse): void => {
           this.addDescriptions(undefined, [resp.Info])
-          done(false, resp.Info)
+          done?.(false, resp.Info)
         }, () => {
-          done(true)
+          done?.(true)
         })
     },
-    adminGetDescriptions (req: AdminGetDescriptionsRequest, done: (error: boolean, rows?: Array<Description>, total?: number) => void) {
+    adminGetDescriptions (req: AdminGetDescriptionsRequest, done?: (error: boolean, rows?: Array<Description>, total?: number) => void) {
       doActionWithError<AdminGetDescriptionsRequest, AdminGetDescriptionsResponse>(
         API.ADMIN_GET_GOOD_DESCRIPTIONS,
         req,
         req.Message,
         (resp: AdminGetDescriptionsResponse): void => {
           this.addDescriptions(undefined, resp.Infos)
-          done(false, resp.Infos, resp.Total)
+          done?.(false, resp.Infos, resp.Total)
         }, () => {
-          done(true)
+          done?.(true)
         })
     },
-    adminUpdateDescriptions (req: AdminUpdateDescriptionRequest, done: (error: boolean, row?: Description) => void) {
+    adminUpdateDescription (req: AdminUpdateDescriptionRequest, done?: (error: boolean, row?: Description) => void) {
       doActionWithError<AdminUpdateDescriptionRequest, AdminUpdateDescriptionResponse>(
         API.ADMIN_UPDATE_GOOD_DESCRIPTION,
         req,
         req.Message,
         (resp: AdminUpdateDescriptionResponse): void => {
           this.addDescriptions(undefined, [resp.Info])
-          done(false, resp.Info)
+          done?.(false, resp.Info)
         }, () => {
-          done(true)
+          done?.(true)
         })
     },
-    adminDeleteDescriptions (req: AdminDeleteDescriptionRequest, done: (error: boolean, row?: Description) => void) {
+    adminDeleteDescription (req: AdminDeleteDescriptionRequest, done?: (error: boolean, row?: Description) => void) {
       doActionWithError<AdminDeleteDescriptionRequest, AdminDeleteDescriptionResponse>(
         API.ADMIN_DELETE_GOOD_DESCRIPTION,
         req,
         req.Message,
         (resp: AdminDeleteDescriptionResponse): void => {
           this._deleteDescription(undefined, resp.Info)
-          done(false, resp.Info)
+          done?.(false, resp.Info)
         }, () => {
-          done(true)
+          done?.(true)
         })
     }
   }
---
+++ sdk/gooddescription.ts
@@ -0,0 +1,163 @@
+import { computed } from 'vue'
+import { appgooddescription, constant, notify } from '..'
+import { AppID } from './localapp'
+
+const description = appgooddescription.useDescriptionStore()
+
+const getPageDescriptions = (pageIndex: number, pageEnd: number, done?: (error: boolean, totalPages: number, totalRows: number) => void) => {
+  description.getDescriptions({
+    Offset: pageIndex * constant.DefaultPageSize,
+    Limit: constant.DefaultPageSize,
+    Message: {
+      Error: {
+        Title: 'MSG_GET_DESCRIPTIONS',
+        Message: 'MSG_GET_DESCRIPTIONS_FAIL',
+        Popup: true,
+        Type: notify.NotifyType.Error
+      }
+    }
+  }, (error: boolean, rows?: Array<appgooddescription.Description>, total?: number) => {
+    if (error || !rows?.length || (pageEnd > 0 && pageIndex === pageEnd - 1)) {
+      const totalPages = Math.ceil(total as number / constant.DefaultPageSize)
+      done?.(error, totalPages, total as number)
+      return
+    }
+    getPageDescriptions(++pageIndex, pageEnd, done)
+  })
+}
+
+export const getGoodDescriptions = (pageStart: number, pages: number, done?: (error: boolean, totalPages: number, totalRows: number) => void) => {
+  getPageDescriptions(pageStart, pages ? pageStart + pages : pages, done)
+}
+
+const adminGetPageDescriptions = (pageIndex: number, pageEnd: number, done?: (error: boolean, totalPages: number, totalRows: number) => void) => {
+  description.adminGetDescriptions({
+    Offset: pageIndex * constant.DefaultPageSize,
+    Limit: constant.DefaultPageSize,
+    TargetAppID: AppID.value,
+    Message: {
+      Error: {
+        Title: 'MSG_GET_DESCRIPTIONS',
+        Message: 'MSG_GET_DESCRIPTIONS_FAIL',
+        Popup: true,
+        Type: notify.NotifyType.Error
+      }
+    }
+  }, (error: boolean, rows?: Array<appgooddescription.Description>, total?: number) => {
+    if (error || !rows?.length || (pageEnd > 0 && pageIndex === pageEnd - 1)) {
+      const totalPages = Math.ceil(total as number / constant.DefaultPageSize)
+      done?.(error, totalPages, total as number)
+      return
+    }
+    adminGetPageDescriptions(++pageIndex, pageEnd, done)
+  })
+}
+
+export const adminGetGoodDescriptions = (pageStart: number, pages: number, done?: (error: boolean, totalPages: number, totalRows: number) => void) => {
+  adminGetPageDescriptions(pageStart, pages ? pageStart + pages : pages, done)
+}
+
+export const goodDescriptions = computed(() => description.descriptions(AppID.value))
+
+export const createGoodDescription = (target: appgooddescription.Description, done?: (error: boolean) => void) => {
+  description.createDescription({
+    ...target,
+    Message: {
+      Error: {
+        Title: 'MSG_CREATE_DESCRIPTION',
+        Message: 'MSG_CREATE_DESCRIPTION_FAIL',
+        Popup: true,
+        Type: notify.NotifyType.Error
+      },
+      Info: {
+        Title: 'MSG_CREATE_DESCRIPTION',
+        Message: 'MSG_CREATE_DESCRIPTION_SUCCESS',
+        Popup: true,
+        Type: notify.NotifyType.Success
+      }
+    }
+  }, done)
+}
+
+export const updateGoodDescription = (target: appgooddescription.Description, done?: (error: boolean) => void) => {
+  description.updateDescription({
+    ...target,
+    Message: {
+      Error: {
+        Title: 'MSG_UPDATE_DESCRIPTION',
+        Message: 'MSG_UPDATE_DESCRIPTION_FAIL',
+        Popup: true,
+        Type: notify.NotifyType.Error
+      },
+      Info: {
+        Title: 'MSG_UPDATE_DESCRIPTION',
+        Message: 'MSG_UPDATE_DESCRIPTION_SUCCESS',
+        Popup: true,
+        Type: notify.NotifyType.Success
+      }
+    }
+  }, done)
+}
+
+export const adminUpdateGoodDescription = (target: appgooddescription.Description, done?: (error: boolean) => void) => {
+  description.adminUpdateDescription({
+    ...target,
+    TargetAppID: target.AppID,
+    Message: {
+      Error: {
+        Title: 'MSG_UPDATE_DESCRIPTION',
+        Message: 'MSG_UPDATE_DESCRIPTION_FAIL',
+        Popup: true,
+        Type: notify.NotifyType.Error
+      },
+      Info: {
+        Title: 'MSG_UPDATE_DESCRIPTION',
+        Message: 'MSG_UPDATE_DESCRIPTION_SUCCESS',
+        Popup: true,
+        Type: notify.NotifyType.Success
+      }
+    }
+  }, done)
+}
+
+export const deleteGoodDescription = (target: appgooddescription.Description, done?: (error: boolean) => void) => {
+  description.deleteDescription({
+    ...target,
+    Message: {
+      Error: {
+        Title: 'MSG_DELETE_DESCRIPTION',
+        Message: 'MSG_DELETE_DESCRIPTION_FAIL',
+        Popup: true,
+        Type: notify.NotifyType.Error
+      },
+      Info: {
+        Title: 'MSG_DELETE_DESCRIPTION',
+        Message: 'MSG_DELETE_DESCRIPTION_SUCCESS',
+        Popup: true,
+        Type: notify.NotifyType.Success
+      }
+    }
+  }, done)
+}
+
+export const adminDeleteGoodDescription = (target: appgooddescription.Description, done?: (error: boolean) => void) => {
+  description.adminDeleteDescription({
+    ID: target.ID,
+    EntID: target.EntID,
+    TargetAppID: target.AppID,
+    Message: {
+      Error: {
+        Title: 'MSG_DELETE_DESCRIPTION',
+        Message: 'MSG_DELETE_DESCRIPTION_FAIL',
+        Popup: true,
+        Type: notify.NotifyType.Error
+      },
+      Info: {
+        Title: 'MSG_DELETE_DESCRIPTION',
+        Message: 'MSG_DELETE_DESCRIPTION_SUCCESS',
+        Popup: true,
+        Type: notify.NotifyType.Success
+      }
+    }
+  }, done)
+}
