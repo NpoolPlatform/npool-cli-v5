85313e113b93c4ca41cbf18d9e5df027749401cb
lidaqiang <dqli161@163.com>
2024-08-15 10:49:16 +0800

    fix update role

--- appuser/role/const.ts
+++ appuser/role/const.ts
@@ -9,6 +9,7 @@ export enum API {
   GET_APP_USERS = '/appuser/v1/get/app/users',
   GET_APP_ROLES = '/appuser/v1/get/app/roles',
   CREATE_APP_ROLE = '/appuser/v1/create/app/role',
+  UPDATE_APP_ROLE = '/appuser/v1/update/app/role',
   GET_APP_ROLE_USERS = '/appuser/v1/get/app/roleusers',
   CREATE_APP_ROLE_USER = '/appuser/v1/create/app/roleuser',
   DELETE_APP_ROLE_USER = '/appuser/v1/delete/app/roleuser',
