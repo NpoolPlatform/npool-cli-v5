57661e7cde8d38b521af569e9336ff9cb6fc54a5
jakys <jakysing@163.com>
2025-01-15 11:50:15 +0800

    Rename files

--- good/pledge/const.ts
+++
@@ -1,7 +0,0 @@
-export enum API {
-  ADMIN_CREATE_PLEDGE = '/good/v1/admin/create/pledge',
-  ADMIN_UPDATE_PLEDGE = '/good/v1/admin/update/pledge',
-  GET_PLEDGE = '/good/v1/get/pledge',
-  GET_PLEDGES = '/good/v1/get/pledges',
-  ADMIN_DELETE_PLEDGE = '/good/v1/admin/delete/pledge'
-}
